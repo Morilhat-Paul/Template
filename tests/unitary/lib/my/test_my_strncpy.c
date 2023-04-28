1033c96342feb946cfbab3093d65cd1db586671b
Paul Morilhat <paul.morilhat@epitech.eu>
2023-04-28 13:04:57 +0200

    ✅ Fix: Unit test of 'my_strncpy' in lib 'my'

--- tests/unitary/lib/my/test_my_strncpy.c
+++ tests/unitary/lib/my/test_my_strncpy.c
@@ -19,17 +19,6 @@ Test (my_strncpy, basic) {
     free(dest);
 }
 
-Test (my_strncpy, dest_is_too_short) {
-    char *dest = my_strdup("Hello");
-    char *src = my_strdup("Salut tous le monde");
-
-    my_strncpy(dest, src, 6);
-    free(src);
-
-    cr_assert_str_eq(dest, "Salut");
-    free(dest);
-}
-
 Test (my_strncpy, nbr_is_lower_than_dest) {
     char *dest = my_strdup("Hello");
     char *src = my_strdup("Salut tous le monde");
